55a4b3e0c49247fec491bbd72d9aa9daba32962c
Gustavo Tammenhain <gustavotammenhain@gmail.com>
2023-02-21 23:49:51 -0300

    Ajuste componentes

--- src/app/components/return-button/return-button.component.ts
+++ src/app/components/return-button/return-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-return-button',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReturnButtonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+  
+  goToRoute(route: any){
+      
+    this.router.navigate([route])
 
+  }
 }
